afade8c5033f474efae48d8262cde26d6dba05ed
lioipu <57569699+lioipu@users.noreply.github.com>
2025-03-17 00:00:48 +0200

    fix: add if statement for handle remove functions to make sure not to remove an item from totalItems arr before his amount property is 0
    refactor: adding popup window for compliting an order

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {menuArray} from '/data.js'
 
 let totalItems = []
-let cnt = 0
+let isClicked = false
 
 document.addEventListener('click', e => {
     if(e.target.dataset.addItemBtn === '0'){
@@ -16,42 +16,78 @@ document.addEventListener('click', e => {
         handleRemoveClickHamburgerBtn()
     } else if(e.target.dataset.rmItemBtn === '2'){
         handleRemoveClickBeerBtn()
+    } else if(e.target.id === "complete-order-btn"){
+        handleClickCompleteOrderBtn()
+    } else if(!e.target.dataset.cardDetails){
+        handleClickAnywhere()
     }
 })
 
 function handlePizzaClickItemBtn(){
     menuArray[0].amount++
+    if(!totalItems.includes(menuArray[0])){
+        totalItems.push(menuArray[0])
+    }
     render()
 }
 
 function handleHamburgerClickItemBtn(){
     menuArray[1].amount++
+    if(!totalItems.includes(menuArray[1])){
+        totalItems.push(menuArray[1])
+    }
     render()
-    
 }
 
 function handleBeerClickItemBtn(){
     menuArray[2].amount++
+    if(!totalItems.includes(menuArray[2])){
+        totalItems.push(menuArray[2])
+    }
     render()
-
 }
 
 function handleRemoveClickPizzaBtn(){
-    menuArray[0].amount--
-    render()
-}
-function handleRemoveClickHamburgerBtn(){
-    menuArray[1].amount--
-    render()
-}
-function handleRemoveClickBeerBtn(){
-    menuArray[2].amount--
+    if( ( --menuArray[0].amount ) < 1){
+        totalItems.splice(totalItems.indexOf(menuArray[0]), 1)
+    }
     render()
 }
 
+function handleRemoveClickHamburgerBtn(){
+    if( ( --menuArray[1].amount ) < 1){
+        totalItems.splice(totalItems.indexOf(menuArray[1]), 1)
+    }
+    render()
+}
+
+function handleRemoveClickBeerBtn(){
+    if( ( --menuArray[2].amount ) < 1){
+        totalItems.splice(totalItems.indexOf(menuArray[2]), 1)
+    }
+    render()
+}
+
+function handleClickCompleteOrderBtn(){
+    if(isClicked){
+        isClicked = false
+    } else {
+        isClicked = true
+    }
+    render()    
+}
+
+function handleClickAnywhere(){
+    isClicked = false
+    render()
+}
 
 function getOrderListHtml() {
-
+    let isHidden = 'hidden'
+    if(isClicked){
+        isHidden = ''
+    }
+    
     let feed = menuArray.map(item => {
             return `<div class="container">
                 <p id="logo">${item.emoji}</p>
@@ -63,50 +99,52 @@ function getOrderListHtml() {
                 <button id="add-item-btn" data-add-item-btn="${item.id}">+</button>
             </div>`
         }).join('')
-        
-        if(menuArray[0].amount > 0 || menuArray[1].amount > 0 || menuArray[2].amount > 0 ){
+
+        if(totalItems.length > 0){
+            feed += `<div id="total-order">
+                        <div id="order-items">
+                            <h1>Your Order</h1>`
             feed += 
-            `<div id="total-order">
-                <div id="order-items">
-                    <h1>Your Order</h1>`
-            if(menuArray[0].amount > 0){
-                feed += 
-                `
-                <div class="item-content-sum">
-                    <p id="name">${menuArray[0].name}</p>
-                    <button id="remove-btn" data-rm-item-btn="${menuArray[0].id}">remove</button>
-                    <p id="price">$${menuArray[0].price * menuArray[0].amount}</p>
-                </div>`
-            } 
-            if(menuArray[1].amount > 0){
-                feed += 
-                `
-                <div class="item-content-sum">
-                    <p id="name">${menuArray[1].name}</p>
-                    <button id="remove-btn" data-rm-item-btn="${menuArray[1].id}">remove</button>
-                    <p id="price">$${menuArray[1].price * menuArray[1].amount}</p>
-                </div>`
-            } 
-            if(menuArray[2].amount > 0) {
-                feed += 
-                `
-                <div class="item-content-sum">
-                    <p id="name">${menuArray[2].name}</p>
-                    <button id="remove-btn" data-rm-item-btn="${menuArray[2].id}">remove</button>
-                    <p id="price">$${menuArray[2].price * menuArray[2].amount}</p
-                </div>
-                </div>`
-            }
-            let sum =   menuArray[0].price * menuArray[0].amount +
-                        menuArray[1].price * menuArray[1].amount +
-                        menuArray[2].price * menuArray[2].amount
+            totalItems.map(item => {
+                return `
+                        <div class="item-content-sum">
+                            <p id="name">${item.name}</p>
+                            <button id="remove-btn" data-rm-item-btn="${item.id}">remove</button>
+                            <p id="price">$${item.price * item.amount}</p>
+                        </div>`
+            }).join('')
+            let sum =  totalItems.reduce( (accumulator, currentVal) => {
+                return accumulator + ( currentVal.price * currentVal.amount ) 
+            }, 0)
             feed += `
-            <div class="item-content-sum">
-                <p>Total Price:${sum}</p>
-            </div>
+                </div>
+                    <div class="item-content-sum">
+                    <p id="name">Total Price:</p>
+                    <p id="price">$${sum}</p>
+                </div>
+                <div id="complete-order">
+                    <button id="complete-order-btn">Complete order</button>
+                </div>
             </div>`
+
+            
+            
         }
-        return feed
+        feed += `
+        <div id="card-details" class="${isHidden}" data-card-details="card-details">
+            <div id="title" data-card-details="card-details">
+                <p data-card-details="card-details">Enter card details</p>
+            </div>
+            <div id="input-card-info" data-card-details="card-details">
+                <input placeholder="Enter your name" data-card-details="card-details">
+                <input placeholder="Enter your number" data-card-details="card-details">
+                <input placeholder="Enter CVV" data-card-details="card-details">
+            </div>
+            <div id="pay-btn" >
+                <button>Pay</button>
+            </div>
+        </div>`
+    return feed
 }
 
 function render(){
